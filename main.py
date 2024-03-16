8507babfb084a91c44ff5371ed96df2b58bbdf3b
Dunconius <duncanbean84@gmail.com>
2024-03-16 11:26:59 +1100

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask
+#this allows us to handle all the errors at 1 point instead of using try-except everywhere
+from marshmallow.exceptions import ValidationError
+
+from init import db, ma, bcrypt, jwt
+
+def create_app():
+    app = Flask(__name__)
+
+    # this is telling flask not to sort the keys, as it's sorting was overwriting what we told marshmallow to sort.
+    app.json.sort_keys = False
+
+    # configs
+    app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("DATABASE_URI")
+    app.config["JWT_SECRET_KEY"]=os.environ.get("JWT_SECRET_KEY")
+
+    # connect libraries with flask app
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    # global error handlers
+    @app.errorhandler(400)
+    def bad_request(err):
+        return {"error": str(err)}, 400
+    
+    @app.errorhandler(404)
+    def not_found(err):
+        return {"error": str(err)}, 404
+
+    @app.errorhandler(ValidationError)
+    def validation_error(err):
+        return {"error": err.messages}, 400
+    
+    return app
