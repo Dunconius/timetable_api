2de52bd27c925583f230c90fc1fafdd92895272b
Dunconius <duncanbean84@gmail.com>
2024-03-21 18:38:26 +1100

    added schedules controller

--- controllers/rooms_controller.py
+++ controllers/rooms_controller.py
@@ -3,24 +3,22 @@ from flask import Blueprint, request, jsonify
 from init import db
 from models.room import Room, room_schema, rooms_schema
 
-
-
 rooms_bp = Blueprint('rooms', __name__, url_prefix='/rooms')
 
 # get ALL time slots - GET
 @rooms_bp.route('/', methods=['GET'])
-def get_all_times():
-    rooms = TimeSlot.query.all()
-    rooms_list = [{'id': time_slot.id, 'Day': time_slot.time_slot_day, 'Time': time_slot.time_slot_time} for time_slot in rooms]
-    return jsonify({'timeslots': rooms_list})
+def get_all_rooms():
+    rooms = Room.query.all()
+    rooms_list = [{'id': room.id, 'Building': room.building_number, 'Room Number': room.room_number} for room in rooms]
+    return jsonify({'rooms': rooms_list})
 
 # get ONE time slot and show schedules that time.
-@rooms_bp.route('/<int:time_slot_id>')
-def get_one_timeslot(time_slot_id):
-    time_slot = TimeSlot.query.get_or_404(time_slot_id)
+@rooms_bp.route('/<int:room_id>')
+def get_one_room(room_id):
+    room = Room.query.get_or_404(room_id)
 
-    # subjects = [subject_schema.dump(subject) for subject in time_slot.subjects]
+    # subjects = [subject_schema.dump(subject) for subject in room.subjects]
 
     return {
-        "time slot": time_slot_schema.dump(time_slot)       
+        "Room": room_schema.dump(room)       
     }
--- controllers/schedule_controller.py
+++ controllers/schedule_controller.py
@@ -2,4 +2,13 @@ from flask import Blueprint, request
 
 from init import db
 
-from models.schedule import Schedule
+from models.schedule import Schedule, schedule_schema, schedules_schema
+
+schedules_bp = Blueprint('schedules', __name__, url_prefix='/schedules')
+
+# get ALL schedules - GET (schedule, subject, room, timeslot)
+@schedules_bp.route('/', methods=['GET'])
+def get_all_schedules():
+    schedules = Schedule.query.all()
+    schedules_list = [{'id': schedule.id} for schedule in schedules]
+    return jsonify('schedules': schedules_list)
